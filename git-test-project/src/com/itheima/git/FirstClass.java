fcef2522f2fcddb0a59b51f2ca9a67b0373e5c29
kpwang <kpwang321@163.com>
2020-07-12 21:44:43 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author kpwang
+ * @create 2020-07-12 21:32
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
